a7b74656e07e9d751f591d593d33cf533a618be1
Cristina Geremia <112133093+cgeremia@users.noreply.github.com>
2023-01-19 12:53:54 -0500

    redirect to login

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// Redirect to login page
+const withAuth = (req, res, next) => {
+  if (!req.session.user_id) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
